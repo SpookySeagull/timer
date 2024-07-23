88c7c80b3084d4b8fe5e9eb9123c04a8890e97f8
SpookySeagull <igortyvlev@yandex.ru>
2024-07-23 16:00:03 +0300

    fix /sigh

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Timer
 Small timer for using Pomidoro when I study
+
+##TODOs
+
+- [x] Make a test timer
+- [x] Make a test with command-line arguments
+- [ ] Make exeption for no arguments
+- [ ] Get it to make sound
+- [ ] Make it all work together
+- [ ] Create makefile
+- [ ] Spread positivity in the world :)
--- timer.go
+++ timer.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
-	"time"
+	//"time"
+	"os"
 )
 
 func main () {
+	args := os.Args[1:]
+	firstArg := os.Args[1]
+	fmt.Println("All arguments:", args)
+	fmt.Println("First Argument:", firstArg)
+	
+	//TODO: what if there is no arguments?
+
+	/*
 	timer1 := time.NewTimer(2 * time.Second)
 	fmt.Println("Timer starts now")
 	<-timer1.C
 	fmt.Println("Timer ends")
+	*/
 }
 
