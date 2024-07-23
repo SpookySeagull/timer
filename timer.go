2298357fa3de5e573dc1bd5c441d6dc53ca57dee
SpookySeagull <igortyvlev@yandex.ru>
2024-07-23 16:33:18 +0300

    almost ready

--- timer.go
+++ timer.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"fmt"
-	//"time"
+	"time"
 	"os"
+	"strconv"
 )
 
+//convert str from Argument to time.Duration for timer
+func argToTime(str string) time.Duration {
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+	return time.Duration(i) 
+}
+
 func main () {
-	args := os.Args[1:]
-	firstArg := os.Args[1]
-	fmt.Println("All arguments:", args)
-	fmt.Println("First Argument:", firstArg)
-	
+	minute := argToTime(os.Args[1])
+	fmt.Println("Number minutes:", minute)
+
 	//TODO: what if there is no arguments?
 
-	/*
-	timer1 := time.NewTimer(2 * time.Second)
+	timer1 := time.NewTimer(minute * time.Minute)
 	fmt.Println("Timer starts now")
 	<-timer1.C
 	fmt.Println("Timer ends")
-	*/
 }
 
